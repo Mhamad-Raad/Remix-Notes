48f299100d88321855aeb17bb8614ce83302a0ec
joker69216 <hamaraad883@gmail.com>
2023-01-23 10:11:22 +0300

    remove page reload after adding a note

--- app/components/NewNotes.jsx
+++ app/components/NewNotes.jsx
@@ -1,8 +1,10 @@
+import { Form, useNavigation } from '@remix-run/react';
 import styles from './NewNotes.css';
-
 function NewNote() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
-    <form method='post' id='note-form'>
+    <Form method='post' id='note-form'>
       <p>
         <label htmlFor='title'>Title</label>
         <input type='text' id='title' name='title' required />
@@ -12,9 +14,11 @@ function NewNote() {
         <textarea id='content' name='content' rows='5' required />
       </p>
       <div className='form-actions'>
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Adding' : 'Add Note'}
+        </button>
       </div>
-    </form>
+    </Form>
   );
 }
 
