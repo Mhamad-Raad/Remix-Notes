58e70c98fe6f91e8f72b73cd6a47d14a29795737
joker69216 <hamaraad883@gmail.com>
2023-01-29 11:35:48 +0300

    add noteId dynamic page

---
+++ app/routes/Notes.$noteId.jsx
@@ -0,0 +1,24 @@
+import NoteDetails from '~/styles/note-details.css';
+
+export default function NoteDetail() {
+  return (
+    <main id='note-details'>
+      <header>
+        <nav>
+          <Link to='/notes'>Back to all Notes</Link>
+        </nav>
+        <h1>NOTE TITLE</h1>
+      </header>
+      <p id='note-details-content'>NOTE CONTENT</p>
+    </main>
+  );
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: NoteDetails,
+    },
+  ];
+}
